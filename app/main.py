c76f92bb6e212740bde24df4bc2bb925a725c3df
Normis-Skuf <test-email@mail.com>
2026-02-12 16:36:19 +0200

    developed cache decorator in main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
+import functools
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_results = {}
+    functools.wraps(func)
+
+    def wrapper(*args) -> int:
+
+        if args not in cache_results:
+            print("Calculating new result")
+            result = func(*args)
+            cache_results[args] = result
+            return result
+
+        print("Getting from cache")
+        return cache_results[args]
+
+    return wrapper
