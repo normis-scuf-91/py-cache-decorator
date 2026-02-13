277fcb86c65179ba3129a7785f43752406144b28
Normis-Skuf <test-email@mail.com>
2026-02-13 09:03:27 +0200

    refactored wrapper annotation, refactored wrapper accepting arguments

--- app/main.py
+++ app/main.py
@@ -1,19 +1,18 @@
 import functools
 from typing import Callable
+from typing import Any
 
 
 def cache(func: Callable) -> Callable:
     cache_results = {}
     functools.wraps(func)
 
-    def wrapper(*args) -> int:
-
+    def wrapper(*args, **kwargs) -> Any:
         if args not in cache_results:
             print("Calculating new result")
-            result = func(*args)
+            result = func(*args, **kwargs)
             cache_results[args] = result
             return result
-
         print("Getting from cache")
         return cache_results[args]
 
